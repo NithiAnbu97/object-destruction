15441b01595d4e2ded3f607c6d70b84271f84fe7
NithiAnbu97 <96058658+NithiAnbu97@users.noreply.github.com>
2021-12-24 14:09:41 +0530

    Add files via upload
    to find who are passed & failed

---
+++ object destruction.js
@@ -0,0 +1,79 @@
+
+const scores = [
+    {
+      marks: 32,
+      name: "Yvette Merritt"
+    },
+    {
+      marks: 57,
+      name: "Lillian Ellis"
+    },
+    {
+      marks: 22,
+      name: "Mccall Carter"
+    },
+    {
+      marks: 21,
+      name: "Pate Collier"
+    },
+    {
+      marks: 91,
+      name: "Debra Beard"
+    },
+    {
+      marks: 75,
+      name: "Nettie Hancock"
+    },
+    {
+      marks: 20,
+      name: "Hatfield Hodge"
+    }
+  ];
+  // [32, 57, 22]
+  // Task 1
+  // Print the name list - As an array
+  // Expected Output
+  // ["Yvette Merritt", "Lillian Ellis", "Mccall Carter", "Pate Collier" ,
+  //  "Debra Beard", "Nettie Hancock",  "Hatfield Hod
+  
+  const ispass = scores.filter(score => score.marks >= 40);
+const isfail = scores.filter(score => score.marks < 40).map(scores=>scores.name);
+ const grades = scores.map(function(entry)
+ {
+  if(entry.marks<=40)
+  entry.grade='fail';
+  else if(entry.marks>=40)
+  entry.grade='pass';
+  
+  
+  return entry;
+});
+
+console.log(grades);
+  
+  
+  
+  // Task 2
+  // >=40 pass.. find those student that passed (Use array method)
+  // Expected Output
+  // [{
+  //     marks: 57,
+  //     name: "Lillian Ellis"
+  //   },
+  //     {
+  //     marks: 91,
+  //     name: "Debra Beard"
+  //   },
+  //   {
+  //     marks: 75,
+  //     name: "Nettie Hancock"
+  //   }]
+  
+  // Task 3
+  //  Find all the names who failed the exams (Array methods)
+  // Expected Output
+  //   ["Yvette Merritt",
+  //   "Mccall Carter",
+  //   "Pate Collier",
+  //   "Hatfield Hodge"
+  // ];
